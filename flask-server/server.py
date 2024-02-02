5b63a8e67d946a52416ba8a1f728321483dfd506
ScharmhaarKonig <115897427+ScharmhaarKonig@users.noreply.github.com>
2024-02-02 20:06:04 +0300

    Update server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,19 +5,26 @@ app = Flask(__name__)
 CORS(app)
 
 todos = []
+todo_id_counter = 1
 
 @app.route("/todos", methods=["GET", "POST"])
 def handle_todos():
     if request.method == "GET":
         response = jsonify({"todos": todos})
-        response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
         return response
     elif request.method == "POST":
         new_todo = request.json.get("todo")
-        todos.append(new_todo)
-        response = jsonify(new_todo)
-        response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
+        todo = {"id": todo_id_counter, "task": new_todo, "editing": False}
+        todo_id_counter += 1
+        todos.append(todo)
+        response = jsonify(todo)
         return response, 201
+
+@app.route("/todos/<int:todo_id>", methods=["DELETE"])
+def delete_todo(todo_id):
+    global todos
+    todos = [todo for todo in todos if todo.get("id") != todo_id]
+    return jsonify({"message": "Todo deleted"})
 
 @app.route("/todos/<int:todo_id>/toggleEditing", methods=["PUT"])
 def toggle_editing(todo_id):
@@ -27,18 +34,5 @@ def toggle_editing(todo_id):
             return jsonify(todo)
     return jsonify({"error": "Todo not found"}), 404
 
-@app.route("/todos/<int:todo_id>", methods=["DELETE"])
-def delete_todo(todo_id):
-    for todo in todos:
-        if todo.get("id") == todo_id:
-            todos.remove(todo)
-            return jsonify({"message": "Todo deleted"})
-    return jsonify({"error": "Todo not found"}), 404
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
